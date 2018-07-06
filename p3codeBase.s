930650cc885f4b38aa5c6a0f2d600d81e90632f7
Brian Lai <brianlai30@outlook.com>
2018-07-06 10:44:00 -0700

    Add files via upload

---
+++ p3codeBase.s
@@ -0,0 +1,101 @@
+    .data
+        endl:    .asciiz  "\n"   # used for cout << endl;
+        albl:    .asciiz  "Value of a: " # label for a
+        blbl:    .asciiz  "Value of b: " # label for b
+        clbl:    .asciiz  "Value of c: " # label for c
+        dlbl:    .asciiz  "Value of d: " # label for d
+    .text
+
+# a --> $s0
+# b --> $s1
+# c --> $s2
+# d --> $s3
+main:    
+    li   $s0, 5
+    li   $s1, 6
+    li   $s2, 7
+    li   $s3, -1
+
+    bge  $s0, 10, exit0
+    addi $s0, $s0, 1
+    j skip0
+    exit0:
+    addi $s0, $s0, -1
+    skip0:
+
+    add  $s3, $s0, $s2
+    add  $s2, $s0, $s3
+
+    bge  $s1, 10, exit1
+    addi $s1, $s1, 1
+    addi $s2, $s2, -1
+    j skip1
+    exit1:
+    addi $s1, $s1, -1
+    addi $s2, $s2, 1
+    skip1:
+
+    add  $s0, $s2, $s1
+    add  $s1, $s2, $s3
+
+    bge  $s1, $s2, next
+    ble  $s1, $s0, next
+    add  $s3, $s0, $s1
+    next:
+    bgt  $s1, $s2, skip2
+    blt  $s2, $s0, skip2
+    j exit
+    skip2:
+    add  $s3, $s1, $s2
+    
+exit:
+    la   $a0, albl      # puts albl into arg0 (a0 register) for cout
+    addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
+    syscall             # make a syscall to system
+    
+    move $a0, $s0       # puts a into arg0 (a0 register) for cout
+    addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int 
+    syscall             # make a syscall to system
+
+    la   $a0, endl      # puts the address of the string endl into a0
+    addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+    syscall
+
+    la   $a0, blbl      # puts blbl into arg0 (a0 register) for cout
+    addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing an string
+    syscall             # make a syscall to system
+    
+    move $a0, $s1       # puts b into arg0 (a0 register) for cout
+    addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int 
+    syscall             # make a syscall to system
+
+    la   $a0, endl      # puts the address of the string endl into a0
+    addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+    syscall
+
+    la   $a0, clbl      # puts clbl into arg0 (a0 register) for cout
+    addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
+    syscall             # make a syscall to system
+    
+    move $a0, $s2       # puts c into arg0 (a0 register) for cout
+    addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int 
+    syscall             # make a syscall to system
+
+    la   $a0, endl      # puts the address of the string endl into a0
+    addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+    syscall
+
+    la   $a0, dlbl      # puts dlbl into arg0 (a0 register) for cout
+    addi $v0, $0, 4     # puts 4 in v0 which denotes we are printing a string
+    syscall             # make a syscall to system
+    
+    move $a0, $s3       # puts d into arg0 (a0 register) for cout
+    addi $v0, $0, 1     # puts 1 in v0 to denote we are printing an int 
+    syscall             # make a syscall to system
+
+    la   $a0, endl      # puts the address of the string endl into a0
+    addi $v0, $0, 4     # puts 4 into v0 saying we are printing a string
+    syscall
+   
+    addi $v0,$0, 10
+    syscall
